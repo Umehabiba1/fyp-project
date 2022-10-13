1e37745af91de8d7d026911584973678d02053ee
Umehabiba1 <49710498+Umehabiba1@users.noreply.github.com>
2022-10-13 18:49:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from keras.models import load_model
-import cv2  
+from keras.models import load_model  
 import numpy as np 
 from PIL import Image, ImageOps
 
